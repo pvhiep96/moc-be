20ed4fc264a0e2743746876931f52decfa177755
Pham Van Hiep <pvhiep96@gmail.com>
2025-04-26 19:18:28 +0700

    setup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS'].split(',')
-    resource '*',
-      headers: :any,
-      expose: %w[X-Total X-Total-Pages X-Page X-Per-Page X-Next-Page X-Prev-Page X-Offset],
-      methods: %i[get post patch put delete]
+    origins '*'
+    resource '*', headers: :any,
+                  expose:  %w[X-Total X-Total-Pages X-Page X-Per-Page X-Next-Page X-Prev-Page X-Offset],
+                  methods: %i[get post patch put delete]
   end
 end
